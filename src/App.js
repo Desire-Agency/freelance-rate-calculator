19c55dfbd4d942c50c321389a0d0bf063c215256
stefan.vasyliev <stefan.vasyliev@gmail.com>
2025-01-29 18:51:49 -0800

    added changed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import {
   TextField,
   Button,
   Divider,
+  Tooltip,
   Grid,
   Paper,
   Stack,
+  IconButton
 } from "@mui/material";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { motion } from "framer-motion";
 
 const FreelanceRateCalculator = () => {
@@ -17,12 +20,12 @@ const FreelanceRateCalculator = () => {
     rent: "",
     utilities: "",
     other: "",
-    weeklyHours: "",
+    workEveryWeek: "",
   });
   const [errors, setErrors] = useState({});
 
   const formatNumber = (num) => {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    return Math.round(num).toLocaleString();
   };
 
   const handleInputChange = (e) => {
@@ -36,18 +39,18 @@ const FreelanceRateCalculator = () => {
     const stepErrors = {};
 
     if (step === 1) {
-      if (!formData.rent || isNaN(formData.rent) || parseFloat(formData.rent) <= 0) {
+      if (!formData.rent || parseFloat(formData.rent) <= 0) {
         stepErrors.rent = "Enter valid rent or home payment.";
       }
-      if (!formData.utilities || isNaN(formData.utilities) || parseFloat(formData.utilities) <= 0) {
+      if (!formData.utilities || parseFloat(formData.utilities) <= 0) {
         stepErrors.utilities = "Enter valid utilities expense.";
       }
-      if (!formData.other || isNaN(formData.other) || parseFloat(formData.other) <= 0) {
-        stepErrors.other = "Enter valid other expenses (food, insurance, etc.).";
+      if (!formData.other || parseFloat(formData.other) <= 0) {
+        stepErrors.other = "Enter valid other expenses.";
       }
     } else if (step === 2) {
-      if (!formData.weeklyHours || isNaN(formData.weeklyHours) || parseFloat(formData.weeklyHours) <= 0) {
-        stepErrors.weeklyHours = "Enter valid weekly working hours.";
+      if (!(formData.workEveryWeek > 0)) {
+        stepErrors.sickDays = "Enter valid hours per week .";
       }
     }
 
@@ -56,16 +59,19 @@ const FreelanceRateCalculator = () => {
   };
 
   const calculateResults = () => {
-    const monthlyIncome = parseFloat(formData.rent) + parseFloat(formData.utilities) + parseFloat(formData.other);
+    const monthlyIncome =
+      parseFloat(formData.rent || 0) +
+      parseFloat(formData.utilities || 0) +
+      parseFloat(formData.other || 0);
     const yearlyIncome = monthlyIncome * 12;
     const taxAllocation = yearlyIncome * 0.25;
     const postTaxIncome = yearlyIncome - taxAllocation;
-    const totalWorkingHours = parseFloat(formData.weeklyHours) * 52;
+
+    const totalWorkingHours = 52 * (parseFloat(formData.workEveryWeek) || 0);
     const billableHours = totalWorkingHours * 0.6;
-    const hourlyRate = postTaxIncome / billableHours;
+    const hourlyRate = yearlyIncome / billableHours;
 
     return {
-      monthlyIncome,
       yearlyIncome,
       taxAllocation,
       postTaxIncome,
@@ -78,52 +84,85 @@ const FreelanceRateCalculator = () => {
   const renderStep = () => {
     switch (step) {
       case 1:
+        const monthlyIncome = parseFloat(formData.rent || 0) + parseFloat(formData.utilities || 0) + parseFloat(formData.other || 0);
         return (
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <Typography variant="h4" align="center" gutterBottom>
-              Freelance Rate Calculator
-            </Typography>
-            <Divider sx={{ marginBottom: 2 }} />
-            <Typography variant="h6">Monthly Expenses</Typography>
+            <Box style={{ display: "flex", gap: "24px" }}>
+              <TextField
+                fullWidth
+                label="Rent or home payments"
+                name="rent"
+                background="white"
+                value={formData.rent}
+                onChange={handleInputChange}
+                margin="normal"
+                size="small"
+                type="number"
+                error={!!errors.rent}
+                style={{
+                  backgroundColor: "white",
+                  '&:hover': { borderColor: "black" },
+                  '&.Mui-focused': { borderColor: "black" },
+                }}
+              />
+              <TextField
+                fullWidth
+                label="Utilities"
+                name="utilities"
+                value={formData.utilities}
+                onChange={handleInputChange}
+                margin="normal"
+                type="number"
+                error={!!errors.utilities}
+                size="small"
+                style={{
+                  backgroundColor: "white",
+                  '&:hover': { borderColor: "black" },
+                  '&.Mui-focused': { borderColor: "black" },
+                }}
+              />
+            </Box>
             <TextField
               fullWidth
-              label="Rent or Home Payments"
-              name="rent"
-              value={formData.rent}
-              onChange={handleInputChange}
-              margin="normal"
-              type="number"
-              error={!!errors.rent}
-              helperText={errors.rent}
-            />
-            <TextField
-              fullWidth
-              label="Utilities"
-              name="utilities"
-              value={formData.utilities}
-              onChange={handleInputChange}
-              margin="normal"
-              type="number"
-              error={!!errors.utilities}
-              helperText={errors.utilities}
-            />
-            <TextField
-              fullWidth
-              label="Other (Food, Insurance, etc.)"
+              label="Other Expenses (Food/Insurance)"
               name="other"
               value={formData.other}
               onChange={handleInputChange}
               margin="normal"
               type="number"
               error={!!errors.other}
-              helperText={errors.other}
+              size="small"
+              style={{
+                backgroundColor: "white",
+                '&:hover': { borderColor: "black" },
+                '&.Mui-focused': { borderColor: "black" },
+              }}
             />
-            <Box display="flex" justifyContent="space-between" marginTop={2}>
+            <TextField
+              fullWidth
+              label="Minimum Required Monthly Income"
+              value={formatNumber(monthlyIncome)}
+              margin="normal"
+              type="text"
+              size="small"
+              disabled
+              style={{
+                backgroundColor: "#eee",
+                '&:hover': { borderColor: "black" },
+                '&.Mui-focused': { borderColor: "black" },
+              }}
+            />
+            <Box display="flex" justifyContent="end" marginTop={4}>
               <Button
                 variant="contained"
-                color="primary"
+                color="success"
                 onClick={() => {
                   if (validateStep()) setStep(2);
+                }}
+                style={{
+                  borderRadius: "20px",
+                  backgroundColor: "#108a00",
+                  '&:hover': { backgroundColor: "#14A800" },
                 }}
               >
                 Next
@@ -134,38 +173,57 @@ const FreelanceRateCalculator = () => {
       case 2:
         return (
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <Typography variant="h4" align="center" gutterBottom>
-              Freelance Rate Calculator
-            </Typography>
-            <Divider sx={{ marginBottom: 2 }} />
-            <Typography variant="h6">Weekly Working Hours</Typography>
             <TextField
               fullWidth
-              label="Hours Per Week"
-              name="weeklyHours"
-              value={formData.weeklyHours}
+              label="How many hours do you plan to work every week?"
+              name="workEveryWeek"
+              value={formData.workEveryWeek}
               onChange={handleInputChange}
               margin="normal"
+              size="small"
               type="number"
-              error={!!errors.weeklyHours}
-              helperText={errors.weeklyHours}
+              style={{
+                backgroundColor: "white",
+                '&:hover': { borderColor: "black" },
+                '&.Mui-focused': { borderColor: "black" },
+              }}
             />
-            <Typography marginTop={2}>
-              Total Working Hours Per Year: {formatNumber(parseFloat(formData.weeklyHours || 0) * 52)}
-            </Typography>
-            <Box display="flex" justifyContent="space-between" marginTop={2}>
+            <TextField
+              size="small"
+              fullWidth
+              label="Total Working Hours/Year"
+              value={formatNumber((parseFloat(formData.workEveryWeek || 0)) * 52)}
+              margin="normal"
+              type="text"
+              disabled
+              style={{
+                marginTop: "16px",
+                backgroundColor: "#eee",
+                '&:hover': { borderColor: "black" },
+                '&.Mui-focused': { borderColor: "black" },
+              }}
+            />
+            <Box display="flex" justifyContent="space-between" marginTop={4}>
               <Button
-                variant="outlined"
-                color="primary"
+                variant="contained"
                 onClick={() => setStep(1)}
+                style={{
+                  backgroundColor: "#108a00",
+                  borderRadius: "20px",
+                  '&:hover': { backgroundColor: "#14A800" },
+                }}
               >
                 Back
               </Button>
               <Button
                 variant="contained"
-                color="primary"
                 onClick={() => {
                   if (validateStep()) setStep(3);
+                }}
+                style={{
+                  backgroundColor: "#108a00",
+                  borderRadius: "20px",
+                  '&:hover': { backgroundColor: "#14A800" },
                 }}
               >
                 Calculate
@@ -177,28 +235,70 @@ const FreelanceRateCalculator = () => {
         const results = calculateResults();
         return (
           <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <Typography variant="h4" align="center" gutterBottom>
-              Results
-            </Typography>
-            <Divider sx={{ marginBottom: 2 }} />
-            <Typography variant="h5" align="center" gutterBottom>
-              Your Estimated Minimum Hourly Rate: ${formatNumber(results.hourlyRate)}
-            </Typography>
-            <Stack spacing={2} sx={{ padding: 2, backgroundColor: "#f9f9f9", borderRadius: "8px" }}>
-              <Box display="flex" justifyContent="space-between">
-                <Typography>Estimated Yearly Income (Pre-Tax):</Typography>
-                <Typography>${formatNumber(results.yearlyIncome)}</Typography>
+            <Stack spacing={2} style={{ borderRadius: "8px", marginTop: 4 }}  >
+              <Box style={{ margin: "10px 0" }} display="flex" alignItems="center" justifyContent="space-between" marginBottom="35px">
+                <Box style={{ fontSize: "56px", color: "#001E00", }}>${formatNumber(results.hourlyRate)}</Box>
+                <img style={{ height: "100%", maxHeight: "80px", display: "block" }} src="images/img.png" alt="" />
               </Box>
-              <Box display="flex" justifyContent="space-between">
-                <Typography>Tax Allocation (25%):</Typography>
-                <Typography>${formatNumber(results.taxAllocation)}</Typography>
+              <Divider style={{ marginTop: "32px" }} />
+              <Box style={{ margin: "10px 0" }} gap="24px" display="flex" justifyContent="start" marginTop={10} alignItems="center">
+                <Box style={{ background: "#eee", width: "100px", minWidth: "100px", height: "34px", borderRadius: "10px", border: "1px solid D5E0D5" }} display="flex" justifyContent="center" alignItems="center">${formatNumber(results.yearlyIncome)}</Box>
+                <Typography>Estimated minimum required yearly income (pre-tax)</Typography>
               </Box>
-              <Box display="flex" justifyContent="space-between">
-                <Typography>Estimated Yearly Income (Post-Tax):</Typography>
-                <Typography>${formatNumber(results.postTaxIncome)}</Typography>
+              <Divider style={{ margin: "0px" }} />
+              <Box style={{ margin: "10px 0" }} gap="24px" display="flex" justifyContent="start" marginTop={10} alignItems="center">
+                <Box style={{ background: "#eee", width: "100px", minWidth: "100px", height: "34px", borderRadius: "10px", border: "1px solid D5E0D5" }} display="flex" justifyContent="center" alignItems="center">${formatNumber(results.taxAllocation)}</Box>
+                <Typography>Estimated tax allocation set aside (25%)</Typography>
               </Box>
-            </Stack>
-          </motion.div>
+              <Divider style={{ margin: "0px" }} />
+              <Box style={{ margin: "10px 0" }} gap="24px" display="flex" justifyContent="start" marginTop={10} alignItems="center">
+                <Box style={{ background: "#eee", width: "100px", minWidth: "100px", height: "34px", borderRadius: "10px", border: "1px solid D5E0D5" }} display="flex" justifyContent="center" alignItems="center">${formatNumber(results.postTaxIncome)}</Box>
+                <Typography>Estimated minimum required yearly income (post-tax)</Typography>
+              </Box>
+              <Divider style={{ margin: "0px" }} />
+              <Box style={{ margin: "10px 0" }} gap="24px" display="flex" justifyContent="start" marginTop={10} alignItems="center">
+                <Box style={{ background: "#eee", width: "100px", minWidth: "100px", height: "34px", borderRadius: "10px", border: "1px solid D5E0D5" }} display="flex" justifyContent="center" alignItems="center">{formatNumber(results.totalWorkingHours)}</Box>
+                <Typography>Estimated working hours per year</Typography>
+              </Box>
+              <Divider style={{ margin: "0px" }} />
+              <Box style={{ margin: "10px 0" }} gap="24px" display="flex" justifyContent="start" marginTop={10} alignItems="center">
+                <Box style={{ background: "#eee", width: "100px", minWidth: "100px", height: "34px", borderRadius: "10px", border: "1px solid D5E0D5" }} display="flex" justifyContent="center" alignItems="center">{formatNumber(results.billableHours)}</Box>
+                <Typography>Estimated billable hours
+                  <Tooltip
+                    title="To help determine your estimate for your total annual billable hours, let's start with how much you want to work. That means looking at the days you'd want to spend doing other things. You may be familiar with this equation: 40 hours/week x 52 weeks/year = 2,080 hours/year. This equation shows that if you work the conventional 40-hour workweek, 52 weeks per year without breaks, you will work 2,080 hours per year. On average, only 60% of your working hours will be billable for clients, while roughly 40% of the time will be spent on administrative tasks such as client sourcing. Calculate Again Create your freelance profile today Find Work"
+                  >
+                    <IconButton>
+                      <InfoOutlinedIcon width="10px" />
+                    </IconButton>
+                  </Tooltip>
+                </Typography>
+              </Box>
+              <Divider style={{ margin: "0px" }} />
+            </Stack >
+            <Box display="flex" justifyContent="center" marginTop={4}>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={() => {
+                  setFormData({
+                    rent: "",
+                    utilities: "",
+                    other: "",
+                    holidays: "",
+                    sickDays: "",
+                  });
+                  setStep(1);
+                }}
+                style={{
+                  backgroundColor: "#108a00",
+                  borderRadius: "20px",
+                  '&:hover': { backgroundColor: "#14A800" },
+                }}
+              >
+                Calculate again
+              </Button>
+            </Box>
+          </motion.div >
         );
       default:
         return null;
@@ -206,8 +306,28 @@ const FreelanceRateCalculator = () => {
   };
 
   return (
-    <Grid container justifyContent="center" alignItems="center" style={{ height: "100vh" }}>
-      <Paper elevation={3} style={{ padding: "20px", maxWidth: "400px", width: "100%", borderRadius: "10px" }}>
+    <Grid container justifyContent="center" alignItems="center" style={{ height: "100%", minHeight: "100dvh", padding: "15px" }}>
+      <Paper elevation={3} style={{ padding: "24px", maxWidth: "652px", width: "100%", borderRadius: "10px", background: "#F2F7F2", margin: "auto" }}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          style={{ color: "#001e00", fontWeight: 500, marginBottom: "15px" }}
+        >
+          Freelance Rate Calculator
+        </Typography>
+        <Typography
+          variant="body1"
+          gutterBottom
+          style={{ color: "#5e6d55", fontWeight: 400, fontSize: "16px", marginBottom: "45px" }}
+        >
+          Are you new to freelancing or re-evaluating your current hourly rate for your freelance
+          business? Use our free tool to quickly learn tips on how to establish your minimum
+          hourly freelance rate.
+        </Typography>
+        <Typography variant="h6" style={{ color: "#001e00", fontWeight: 400, fontSize: "22px" }} display="flex" justifyContent="space-between" alignItems="center">
+          <Box>{step !== 3 ? "Monthly Expenses" : "Your Estimated Minimum Hourly Rate"}</Box> <Box style={{ fontSize: "20px", textAlign: "end", minWidth: "60px", width: "60px" }}>{step} / 3</Box>
+        </Typography>
+        <Divider style={{ marginBottom: "10px" }} />
         {renderStep()}
       </Paper>
     </Grid>
